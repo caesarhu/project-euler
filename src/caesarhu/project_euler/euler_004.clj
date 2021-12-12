6f2672d208ae9483de8e52055326474139153901
caesarhu <hupeishun@gmail.com>
2021-12-12 15:43:43 +0800

    update euler-004

--- src/caesarhu/project_euler/euler_004.clj
+++ src/caesarhu/project_euler/euler_004.clj
@@ -1,46 +1,28 @@
 (ns caesarhu.project-euler.euler-004
-  (:require [caesarhu.shun-tools.pollard-rho :as rho]
-            [clojure.math.combinatorics :as comb]
+  (:require [clojure.math.numeric-tower :as math]
+            [clojure.math.combinatorics :as combo]
             [caesarhu.shun-tools.math-misc :as misc]))
 
-
-(defn palindrome?
-  [coll]
-  (= coll (reverse coll)))
-
-(defn palindrome-number?
+(defn palindrome-generator
   [n]
-  (palindrome? (seq (str n))))
+  (let [first-digit (range 9 0 -1)
+        other-digit (range 9 -1 -1)]
+    (->> (cons first-digit (repeat (dec n) other-digit))
+         (apply combo/cartesian-product)
+         (map (fn [coll]
+                (concat coll (reverse coll))))
+         (map misc/to-number))))
 
-(defn brute-force
-  [digits]
-  (let [start (apply * (repeat (dec digits) 10))
-        end (apply * (repeat digits 10))]
-    (->> (for [i (range start end)
-               j (range (inc i) end)
-               :let [sum (* i j)]
-               :when (palindrome-number? sum)]
-           sum)
-         (apply max))))
+(defn valid-digits?
+  [d n]
+  (let [valid? (fn [x] (>= (dec (math/expt 10 d)) x (math/expt 10 (dec d))))
+        divisors (misc/divisors n)]
+    (some #(valid? (quot n %)) (filter valid? divisors))))
 
 (defn solve
-  [digits]
-  (let [start (apply * (repeat (dec digits) 10))
-        end (apply * (repeat digits 10))
-        valid-digits? (fn [n] (and (>= n start) (< n end)))
-        valid-number? (fn [n]
-                        (first (for [d (rho/divisors n)
-                                     :when (valid-digits? d)
-                                     :let [q (quot n d)]
-                                     :when (valid-digits? q)]
-                                 n)))]
-    (first (for [n (range (misc/to-number (repeat (* 2 digits) 9)) (misc/to-number (repeat (dec (* 2 digits)) 9)) -1)
-                 :when (and (palindrome-number? n)
-                            (valid-number? n))]
-             n))))
+  [d]
+  (some #(and (valid-digits? d %) %) (palindrome-generator d)))
 
 (comment
   (time (solve 3))
-  (time (brute-force 3))
-  (take 100 (filter palindrome-number? (range 999999 99999 -1)))
   )
