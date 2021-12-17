3a16c3507c3e55c3f7825f086382121910900279
caesarhu <hupeishun@gmail.com>
2021-12-18 05:20:02 +0800

    update julia euler-036

--- src/caesarhu/project_euler/euler_032.clj
+++ src/caesarhu/project_euler/euler_032.clj
@@ -1,15 +1,27 @@
-(ns caesarhu.project-euler.euler-032)
+(ns caesarhu.project-euler.euler-032
+  (:require [caesarhu.shun-tools.math-misc :as misc]))
 
 (defn is-pandigital? [s]
   (= "123456789" (apply str (sort s))))
 
-(defn solve []
+(defn brute-force []
   (reduce +
           (distinct
             (for [a (range 2 5000)
                   b (range a (/ 9999 a))
                   :when (is-pandigital? (str a b (* a b)))]
               (* a b)))))
+
+(defn solve
+  []
+  (let [pandigital (atom #{})]
+    (doseq [i (range 1000 10000)]
+      (let [ds (->> (misc/divisors i) rest butlast vec)
+            length (count ds)]
+        (doseq [j (range (/ length 2))]
+          (when (is-pandigital? (str i (ds j) (ds (- length j 1))))
+            (swap! pandigital conj i)))))
+    (apply + @pandigital)))
 
 (comment
   (time (solve))
