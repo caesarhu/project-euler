00353bb9f7269e8a3fdb114ed31b42eaf90187e3
caesarhu <hupeishun@gmail.com>
2021-12-21 16:50:57 +0800

    update euler-043

--- src/caesarhu/project_euler/euler_043.clj
+++ src/caesarhu/project_euler/euler_043.clj
@@ -1,5 +1,8 @@
 (ns caesarhu.project-euler.euler-043
-  (:require [caesarhu.shun-tools.math-misc :as misc]))
+  (:require [caesarhu.shun-tools.math-misc :as misc]
+            [caesarhu.shun-tools.or-tools.sat :as sat])
+  (:import [com.google.ortools.sat CpModel IntVar CpSolverSolutionCallback LinearExpr]
+           [com.google.ortools.util Domain]))
 
 (defn is-different?
   [v]
@@ -33,6 +36,32 @@
        (map misc/to-number)
        (apply +)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn solve-sat
+  []
+  (let [model (sat/new-model)
+        primes [2 3 5 7 11 13 17]
+        create-domain (fn [p]
+                        (let [array (->> (range)
+                                         (map #(* p %))
+                                         (take-while #(< % 1000))
+                                         long-array)]
+                          (Domain/fromValues array)))
+        vars (->> (repeatedly 9 #(.newIntVar model 0 9 ""))
+                  (cons (.newIntVar model 1 9 ""))
+                  vec)]
+    (.addAllDifferent model (into-array vars))
+    (doseq [i (range 7)
+            :let [sub-vars (subvec vars (inc i) (+ i 4))
+                  p (primes i)]]
+      (.addLinearExpressionInDomain model (LinearExpr/scalProd (into-array sub-vars) (long-array [100 10 1])) (create-domain p)))
+    (->> (sat/solutions model (into-array vars))
+         :values
+         (map misc/to-number)
+         (apply +))))
+
 (comment
   (time (solve))
+  (time (solve-sat))
   )
