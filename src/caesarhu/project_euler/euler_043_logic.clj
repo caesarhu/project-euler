4d17082994a1e540494bf60fc1289f2e8f6c0571
caesarhu <hupeishun@gmail.com>
2021-12-23 04:32:51 +0800

    update euler-043

---
+++ src/caesarhu/project_euler/euler_043_logic.clj
@@ -0,0 +1,39 @@
+(ns caesarhu.project-euler.euler-043-logic
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic :refer :all]
+            [clojure.core.logic.fd :as fd]
+            [caesarhu.shun-tools.math-misc :as misc]))
+
+(defn d3-divisible
+  [a b c p]
+  (fd/eq
+   (= (* (/ (+ (* a 100) (* b 10) c) p) p) (+ (* a 100) (* b 10) c))))
+
+(defn logic
+  []
+  (let [vars (vec (repeatedly 10 lvar))
+        primes [2 3 5 7 11 13 17]
+        rules (vec (for [i (range (count primes))]
+                     (conj (subvec vars (inc i) (+ i 4)) (primes i))))]
+    (run* [q]
+          (== q vars)
+          (everyg #(fd/in % (fd/interval 9)) vars)
+          (fd/distinct vars)
+          (fd/> (vars 0) 0)
+          (apply d3-divisible (rules 0))
+          (apply d3-divisible (rules 1))
+          (apply d3-divisible (rules 2))
+          (apply d3-divisible (rules 3))
+          (apply d3-divisible (rules 4))
+          (apply d3-divisible (rules 5))
+          (apply d3-divisible (rules 6)))))
+
+(defn solve
+  []
+  (->> (logic)
+       (map misc/to-number)
+       (apply +)))
+
+(comment
+  (time (solve))
+  )
