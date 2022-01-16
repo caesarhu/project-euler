49eafd93210120c89bd7b184f5edadb936bcf1b2
caesarhu <hupeishun@gmail.com>
2022-01-16 09:46:20 +0800

    add math-tools dependence

--- src/caesarhu/project_euler/euler_003.clj
+++ src/caesarhu/project_euler/euler_003.clj
@@ -1,9 +1,6 @@
 (ns caesarhu.project-euler.euler-003
-  (:require [caesarhu.shun-tools.primes :as p]
-            [caesarhu.shun-tools.math-misc :as misc]
-            [clojure.math.numeric-tower :as math]
-            [caesarhu.shun-tools.pollard-rho :as rho]
-            [criterium.core :refer [bench]]))
+  (:require [caesarhu.math.primes :as p]
+            [clojure.math.numeric-tower :as math]))
 
 (defn prime-factors-of
   [n]
@@ -21,13 +18,6 @@
   [n]
   (apply max (prime-factors-of n)))
 
-(defn solve
-  [n]
-  (apply max (keys (rho/prime-factors n))))
-
 (comment
-  (time (solve 600851475143))
   (time (brute-force 600851475143))
-  (time (rho/prime-factors 600851475143 rho/floyd))
-  (bench (rho/prime-factors (*' 1238926361552897 93461639715357977769163558199606896584051237541638188580280321) rho/floyd))
   )
